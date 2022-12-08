fc6238d68ff88c7cb86b909cbfe65a3ca5240a75
JeffW <116536323+Nxlq@users.noreply.github.com>
2022-12-08 15:01:08 -0500

    add function to generate the computer's choice

---
+++ script.js
@@ -0,0 +1,19 @@
+/* 2 player game, one human one computer
+both players choose between a rock, paper, or scissors
+choices are compared against one another to decide the winner
+rock beats scissors, paper beats rock, scissors beats paper
+winner is declared */
+
+// function to generate the computer's choice
+function getComputersChoice () {
+    const choices = ['rock', 'paper', 'scissors'];
+    const randomNumber = Math.floor(Math.random() * 3);
+    console.log(choices[randomNumber]);
+    return choices[randomNumber];
+}
+
+getComputersChoice();
+getComputersChoice();
+getComputersChoice();
+
+
