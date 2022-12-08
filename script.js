e63cbfd386ea5c56ddd751727fecb07fda526783
JeffW <116536323+Nxlq@users.noreply.github.com>
2022-12-08 16:18:15 -0500

    add function to get the players choice and validate it

--- script.js
+++ script.js
@@ -17,3 +17,34 @@ getComputersChoice();
 getComputersChoice();
 
 
+// function to recieve the player's choice
+function getPlayersChoice () {
+    let playersInput;
+    // get the prompt input and make it all lowercase
+    playersInput = prompt('What is your choice?').toLowerCase();
+
+    // check if it's valid choice selection (return if it is)
+    if (playersInput === 'rock' || playersInput === 'paper' || playersInput === 'scissors') {
+
+          return playersInput;  
+
+    } else {
+        // if not valid then re-prompt with a different message
+        while (playersInput !== 'rock' && playersInput !== 'paper' && playersInput !== 'scissors') {
+            console.log(`not valid ${playersInput}`);
+            playersInput = prompt('Not a valid weapon choice 😡 choose again').toLowerCase();
+        }
+    }
+    
+    
+    // once it is valid then return the players choice
+    console.log(`now a valid choice ${playersInput}`);
+    return playersInput;
+}
+
+getPlayersChoice();
+
+
+// play game 
+// compare choices against one another 
+// declare winner 
