6b07dcf63a3b73d9074338f458a6f6d50256671e
JeffW <116536323+Nxlq@users.noreply.github.com>
2022-12-27 23:30:40 -0500

    add the functionality to play a round of rps w/ UI
    timeout functions are not pretty...

--- script.js
+++ script.js
@@ -3,11 +3,18 @@ both players choose between a rock, paper, or scissors
 choices are compared against one another to decide the winner
 rock beats scissors, paper beats rock, scissors beats paper
 winner is declared */
-const buttons = document.querySelectorAll('button')
+const buttons = document.querySelectorAll('button');
+const textContainer = document.querySelector('.text-container');
+const promptText = document.querySelector('.prompt-text');
+const yourChoiceText = document.createElement('p');
+const cockyNerdsChoice = document.createElement('p');
+const resultsText = document.createElement('p');
 
+
+// play a round of r p s whenever a weapon button is clicked
 buttons.forEach(button => button.addEventListener('click', () => {
     playRound(getComputersChoice(), button.id)
-}))
+}));
 
 
 // function to generate the computer's choice
@@ -15,7 +22,7 @@ function getComputersChoice () {
     const choices = ['rock', 'paper', 'scissors'];
     const randomNumber = Math.floor(Math.random() * 3);
     return choices[randomNumber];
-}
+};
 
 // function to recieve the player's choice
 function getPlayersChoice () {
@@ -34,12 +41,33 @@ function getPlayersChoice () {
         return playersInput;
     };
 
+function addTextContent (el, string){
+    el.textContent += string;
+}
 
 // Play a single round of rock paper scissors
 function playRound (computersChoice, playersChoice){
     // if there is no input
     if(!computersChoice || !playersChoice) return;
+    //clean the choices text everytime a button is pressed
+    yourChoiceText.textContent = '';
+    cockyNerdsChoice.textContent = '';
+    resultsText.textContent = '';
     
+    promptText.textContent = '🤓📣';
+    setTimeout(addTextContent, 300, promptText, 'ROCK! ...');
+    setTimeout(addTextContent, 600, promptText, 'PAPER! ...');
+    setTimeout(addTextContent, 900, promptText, 'SCISSORS! ...');
+    setTimeout(addTextContent, 1200, promptText, 'SHOOT!!!');
+    setTimeout(function(){
+        yourChoiceText.textContent = `You chose: ${playersChoice}`;
+        textContainer.appendChild(yourChoiceText);
+        cockyNerdsChoice.textContent = `Cocky Nerd chose: ${computersChoice}`;
+        textContainer.appendChild(cockyNerdsChoice);
+    }, 1300);
+   
+    
+
     console.log('__________________________________________');
     console.log(`computer's choice: ${computersChoice}`);
     console.log(`your choice: ${playersChoice}`);
@@ -47,6 +75,10 @@ function playRound (computersChoice, playersChoice){
     // if there is a tie
     if (computersChoice === playersChoice){
         console.log(`Draw 😬 you both chose ${computersChoice}`)
+        setTimeout(addTextContent, 1400, resultsText, 'Draw! 🤓 I\'d recommend walking away while you still can...');
+        setTimeout(function(){
+            textContainer.appendChild(resultsText);
+        }, 1400);
         return;
     };
 
@@ -56,11 +88,19 @@ function playRound (computersChoice, playersChoice){
     playersChoice === 'scissors' && computersChoice === 'rock') {
 
         console.log(`You lose 🤣🤣 ${computersChoice} beats ${playersChoice} 🤡🤡🤡🤡`);
+        setTimeout(addTextContent, 1400, resultsText, `🤓📣 Nice choice bozo 🤣 ${computersChoice} beats ${playersChoice} you lose 🤣🤣🤣`);
+        setTimeout(function(){
+            textContainer.appendChild(resultsText);
+        }, 1400);
         return 'computerWins';
     };
 
     // if the player wins
     console.log(`You win 🥳🥳 ${playersChoice} beats ${computersChoice}`);
+    setTimeout(addTextContent, 1400, resultsText, `Wait... that can't be right 😨 ${playersChoice} beats ${computersChoice}... I guess a chump can get lucky every once in a while. Go again.`);
+        setTimeout(function(){
+            textContainer.appendChild(resultsText);
+        }, 1400);
     return 'playerWins';
 }
 
